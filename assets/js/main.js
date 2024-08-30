96d2958bffc4da823f08d5df58b5171bcd5178d3
HossamElharery <elharery7ossam@gmail.com>
2024-08-30 18:35:50 +0300

    v1

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-let menu = document.getElementById("humbergerMenu");
-let sidebar = document.getElementById("sidebarToggle");
-
-menu.addEventListener("click", () => {
-  sidebar.classList.toggle("d-none");
-  sidebar.classList.toggle("fade-in");
-});
